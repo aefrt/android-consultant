dd9da3155543e319413382496cd09de0de96502c
aefrt <vkopchev43@gmail.com>
2022-07-09 11:26:36 +0300

    trying to commit with android studio

---
+++ app/src/main/java/com/example/myapplicationkotlinbook/chatWindow.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationkotlinbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class chatWindow : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_window)
+    }
+}
