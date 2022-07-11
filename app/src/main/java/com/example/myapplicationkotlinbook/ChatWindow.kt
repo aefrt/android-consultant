e18c64c07b5c17bbcf8402a71e583011bd300449
aefrt <vkopchev43@gmail.com>
2022-07-11 14:03:06 +0300

    Я не понимаю почему он ReadME то обновляет

--- app/src/main/java/com/example/myapplicationkotlinbook/ChatWindow.kt
+++ app/src/main/java/com/example/myapplicationkotlinbook/ChatWindow.kt
@@ -7,5 +7,5 @@ class ChatWindow : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_window)
-    }
+    }  // huy
 }
