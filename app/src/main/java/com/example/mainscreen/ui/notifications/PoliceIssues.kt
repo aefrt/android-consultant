161accf24fe37f7486fd66ef44520d91e28cc4df
kirill867 <95954756+kirill867@users.noreply.github.com>
2022-07-12 20:48:27 +0300

    Добавлена часть инструкции

--- app/src/main/java/com/example/mainscreen/ui/notifications/PoliceIssues.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/PoliceIssues.kt
@@ -1,12 +1,19 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class PoliceIssues : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_police_issues)
+    }
+
+    fun poliss(view: View){
+        intent = Intent(this, Police_issues2::class.java)
+        startActivity (intent)
     }
 }
