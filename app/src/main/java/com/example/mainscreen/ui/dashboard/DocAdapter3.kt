0c7aa9e9f15e65a3ce76dc8dcd74e456179edb0b
Dmitriy <100220518+DmitriyLapynin@users.noreply.github.com>
2022-07-13 22:52:55 +0300

    Добавил нажатие на документ в справочнике

--- app/src/main/java/com/example/mainscreen/ui/dashboard/DocAdapter3.kt
+++ app/src/main/java/com/example/mainscreen/ui/dashboard/DocAdapter3.kt
@@ -3,26 +3,43 @@ package com.example.mainscreen.ui.dashboard
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mainscreen.R
 import com.example.mainscreen.databinding.DocItemBinding
 
+interface DocDelegate3{
+    fun openDoc()
+}
+
 class DocAdapter3: RecyclerView.Adapter<DocAdapter3.DocHolder>() {
     var docList3 = ArrayList<Doc>()
 
-    class DocHolder(item: View) : RecyclerView.ViewHolder(item) {
+    //new code
+    private var delegate: DocDelegate3? = null
+
+    fun attachDelegate(delegate: DocDelegate3){
+        this.delegate = delegate
+    }
+    //
+
+    class DocHolder(item: View, val delegate: DocDelegate3?) : RecyclerView.ViewHolder(item) {
         val binding = DocItemBinding.bind(item)
+        val description : CardView = item.findViewById(R.id.card)
         fun bind(doc: Doc) = with(binding) {
             im.setImageResource(doc.imageId)
             tvTitle.text = doc.title
-
+            description.setOnClickListener {
+                delegate?.openDoc()
+                //
+            }
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DocHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.doc_item, parent, false)
-        return DocAdapter3.DocHolder(view)
+        return DocAdapter3.DocHolder(view, delegate)
     }
 
     override fun onBindViewHolder(holder: DocHolder, position: Int) {
--- app/src/main/java/com/example/mainscreen/ui/dashboard/DocAdapter4.kt
+++ app/src/main/java/com/example/mainscreen/ui/dashboard/DocAdapter4.kt
@@ -3,17 +3,35 @@ package com.example.mainscreen.ui.dashboard
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mainscreen.R
 import com.example.mainscreen.databinding.DocItemBinding
 
+interface DocDelegate4{
+    fun openDoc()
+}
 class DocAdapter4: RecyclerView.Adapter<DocAdapter4.DocHolder>(){
     var docList4 = ArrayList<Doc>()
-    class DocHolder(item: View): RecyclerView.ViewHolder(item){
+
+    //new code
+    private var delegate: DocDelegate4? = null
+
+    fun attachDelegate(delegate: DocDelegate4){
+        this.delegate = delegate
+    }
+    //
+
+    class DocHolder(item: View, val delegate: DocDelegate4?): RecyclerView.ViewHolder(item){
         val binding = DocItemBinding.bind(item)
+        val description : CardView = item.findViewById(R.id.card)
         fun bind(doc: Doc) = with(binding){
             im.setImageResource(doc.imageId)
             tvTitle.text = doc.title
+            description.setOnClickListener {
+                delegate?.openDoc()
+                //
+            }
 
         }
 
@@ -21,7 +39,7 @@ class DocAdapter4: RecyclerView.Adapter<DocAdapter4.DocHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DocHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.doc_item, parent, false)
-        return DocAdapter4.DocHolder(view)
+        return DocAdapter4.DocHolder(view, delegate)
     }
 
     override fun onBindViewHolder(holder: DocHolder, position: Int) {
