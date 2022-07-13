0db0e1a6855d53cb724341a1d89e12907e3b0aa9
aefrt <vkopchev43@gmail.com>
2022-07-13 17:56:29 +0300

    Добавил соединение с БД Firebase (вроде рабоатет, но я все ее разбираюсь + пока не доделал интерфейс)

--- app/src/main/java/com/example/mainscreen/Message.kt
+++ app/src/main/java/com/example/mainscreen/Message.kt
@@ -1,15 +1,12 @@
 package com.example.mainscreen
 
-class Message {
-    /* В гайде поля класса объявляются на Java, дальше поменяю код для Kotlin
-    public String userName;
-    public String textMessage;
-    private long messageTime
+class Message(name: String, text: String, time: Long) {
+    public val userName = name;
+    public val textMessage = text;
+    private val messageTime = time;
 
-    // constructors
-    public Message() {}
+    /* public Message() {}
     public Message(String userName, String textMessage) {
         11:24 https://www.youtube.com/watch?v=Q9FxYm8-u7w&list=PL0lO_mIqDDFUXOmQ5c6hurb2Nqvc1uCxe&index=2
-    }
-    */
+    } */
 }
--- app/src/main/java/com/example/mainscreen/ui/fourth/BlankFragment.kt
+++ app/src/main/java/com/example/mainscreen/ui/fourth/BlankFragment.kt
@@ -1,23 +1,26 @@
 package com.example.mainscreen.ui.fourth
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.RelativeLayout
 import com.example.mainscreen.BlankViewModel
+import com.example.mainscreen.Message
 import com.example.mainscreen.R
+import com.example.mainscreen.ui.notifications.PoliceIssues
 import com.firebase.ui.auth.AuthUI
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class BlankFragment : Fragment() {
-
-    private var SIGN_IN_CODE = 1;
-    private lateinit var activity_main : RelativeLayout; // ???
-
     companion object {
         fun newInstance() = BlankFragment()
     }
@@ -28,30 +31,37 @@ class BlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // YouTube
-        // da blin: activity_main = findViewById() 9:50
-
-        if (FirebaseAuth.getInstance().currentUser == null) {
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),
-                SIGN_IN_CODE);
-            print("b kkkk")
-        } else {
-            print("a kkkk") // а куда он принт сделает?
-            // Snackbar.make().show() // ???
-        }
-
-        // Been
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(BlankViewModel::class.java)
+        viewModel = ViewModelProvider(this)[BlankViewModel::class.java]
         // TODO: Use the ViewModel
     }
 
-    // support.zip: нужно прорисовать отдельно без надписи форму ввода, кнопку отправить и облачко тоже без надписи, а плка буду юзать стандартные.
-    // чтобы понять куда прописывать функции почитать про жизненный цикл Fragment
-    // lect2456 for backend
+    // trying io connect to db
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        var buttonSend = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        var editText = view.findViewById<TextInputEditText>(R.id.textInputEditText)
+        var ref = FirebaseDatabase.getInstance().getReference()
+        ref.keepSynced(true)
+
+        buttonSend.setOnClickListener {
+            var message = editText.text.toString().trim()
+            if (message != "") {
+                val mess = Message("vlad", message, 126)
+                ref.child("chat").push().setValue(mess);
+            }
+            editText.setText("")
+        }
+
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            // ?? https://www.youtube.com/watch?v=gFtajvZajrg&list=PL0lO_mIqDDFUXOmQ5c6hurb2Nqvc1uCxe&index=4 27:00
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),  1)
+        } else {
+            // hm
+        }
+    }
 
 }
