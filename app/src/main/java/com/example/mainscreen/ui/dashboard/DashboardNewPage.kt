3c2cbb82bc2cb6549fb9d4c9b32db4a947f03e96
Dmitriy <100220518+DmitriyLapynin@users.noreply.github.com>
2022-07-14 12:36:02 +0300

    Добавил активити отправить по почте и реализовал внутри интерфейс

--- app/src/main/java/com/example/mainscreen/ui/dashboard/DashboardNewPage.kt
+++ app/src/main/java/com/example/mainscreen/ui/dashboard/DashboardNewPage.kt
@@ -1,7 +1,11 @@
 package com.example.mainscreen.ui.dashboard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 import com.example.mainscreen.R
 
 class DashboardNewPage : AppCompatActivity() {
@@ -9,4 +13,10 @@ class DashboardNewPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard_new_page)
     }
+
+    fun navigate_to_mail(view : View){
+        val intent = Intent(this , Send_to_email::class.java)
+        startActivity(intent)
+    }
 }
+
---
+++ app/src/main/java/com/example/mainscreen/ui/dashboard/Send_to_email.kt
@@ -0,0 +1,12 @@
+package com.example.mainscreen.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mainscreen.R
+
+class Send_to_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_to_email)
+    }
+}
