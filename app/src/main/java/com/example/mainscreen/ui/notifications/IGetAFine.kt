4f170f4fa7a663a5753d4a77727033aab5e9b440
Dmitriy <100220518+DmitriyLapynin@users.noreply.github.com>
2022-07-15 19:48:39 +0300

    Реализовал всплывающее окно в 3 пункте и активити в 4.

--- app/src/main/java/com/example/mainscreen/ui/notifications/IGetAFine.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/IGetAFine.kt
@@ -2,11 +2,15 @@ package com.example.mainscreen.ui.notifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class IGetAFine : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_iget_afine)
+    }
+    fun navigate_postanovlenie(view: View) {
+        Postanovlenie().show(supportFragmentManager, "Postanovlenie")
     }
 }
