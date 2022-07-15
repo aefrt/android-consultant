1a5a07f4dd5f8a1952c44f77cc6caac7181b76f8
aefrt <vkopchev43@gmail.com>
2022-07-15 20:19:48 +0300

    Что с ListView?

--- app/src/main/java/com/example/mainscreen/ui/fourth/BlankFragment.kt
+++ app/src/main/java/com/example/mainscreen/ui/fourth/BlankFragment.kt
@@ -1,22 +1,17 @@
 package com.example.mainscreen.ui.fourth
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ListView
-import android.widget.RelativeLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.mainscreen.BlankViewModel
 import com.example.mainscreen.Message
 import com.example.mainscreen.R
-import com.example.mainscreen.ui.notifications.PoliceIssues
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.database.FirebaseListAdapter
 import com.firebase.ui.database.FirebaseListOptions
@@ -25,6 +20,7 @@ import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+
 
 class BlankFragment : Fragment() {
     companion object {
@@ -37,6 +33,20 @@ class BlankFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // сейчас временно pабиваю на работу кнопки и изучаю, как реализовать простейший Adapter
+        // https://www.youtube.com/watch?v=eKYBUuxM3aA почему оно у меня не работает???
+        // вообще а почему у меня ListView пустой на экране а у других на скринах заполненный?
+        val view = inflater.inflate(R.layout.fragment_blank, container, false)
+        val data = ArrayList<String>()
+        data.add("Julien")
+        data.add("Julien")
+        data.add("Julien")
+        data.add("Julien")
+        data.add("Julien")
+        data.add("Julien")
+        val adapter1 = ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, data)
+        val listOfMessages = view.findViewById<ListView>(R.id.messages_list)
+        listOfMessages.setAdapter(adapter1)
         return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
@@ -51,28 +61,14 @@ class BlankFragment : Fragment() {
         val SIGN_IN_CODE = 1
         val fragmentBlank = view.findViewById<ConstraintLayout>(R.layout.fragment_blank)
         val adapter : FirebaseListAdapter<Message>
-        var buttonSend = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        var editText = view.findViewById<TextInputEditText>(R.id.textInputEditText)
-        var ref = FirebaseDatabase.getInstance().reference
-        ref.keepSynced(true)
-
-        buttonSend.setOnClickListener {
-            var message = editText.text.toString().trim()
-            if (message != "") {
-                val mess = Message("vlad", message, 126)
-                ref.child("chat").push().setValue(mess);
-            }
-            editText.setText("")
-        }
 
         fun displayMessages() {
             val listOfMessages = view.findViewById<ListView>(R.id.messages_list)
-
-            val options = FirebaseListOptions.Builder<Message>().setQuery(FirebaseDatabase.getInstance().reference, Message::class.java).setLayout(R.layout.messages_list_item,).build()
+            val options = FirebaseListOptions.Builder<Message>().setQuery(FirebaseDatabase.getInstance().reference, Message::class.java).setLayout(R.layout.messages_list_item).build()
             val adapter = object : FirebaseListAdapter<Message>(options) {
                 override fun populateView(v: View, model: Message, position: Int) {
                     val mess_user : TextView = v.findViewById(R.id.message_user)
-                    var mess_time : TextView = v.findViewById(R.id.message_time)
+                    val mess_time : TextView = v.findViewById(R.id.message_time)
                     val mess_text : TextView = v.findViewById(R.id.message_text)
 
                     mess_user.setText(model.userName)
@@ -83,11 +79,25 @@ class BlankFragment : Fragment() {
             listOfMessages.setAdapter(adapter)
         }
 
+        val buttonSend = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val editText = view.findViewById<TextInputEditText>(R.id.textInputEditText)
+        val ref = FirebaseDatabase.getInstance().reference
+        ref.keepSynced(true)
+
+        buttonSend.setOnClickListener {
+            val message = editText.text.toString().trim()
+            if (message != "") {
+                val mess = Message("vlad", message, 126)
+                ref.child("chat").push().setValue(mess);
+            }
+            editText.setText("")
+        }
+
         if (FirebaseAuth.getInstance().currentUser == null) {
             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE)
         } else {
             Snackbar.make(fragmentBlank, "Вы авторизованы", Snackbar.LENGTH_LONG).show()
-            displayMessages()
+            TODO("displayMessages()")
         }
     }
 
