935820c1708aff7b0a3c3849edd480e54f05afc4
aefrt <vkopchev43@gmail.com>
2022-07-15 14:27:58 +0300

    Реализовал поддержку, но она не работает...

--- app/src/main/java/com/example/mainscreen/Message.kt
+++ app/src/main/java/com/example/mainscreen/Message.kt
@@ -4,9 +4,4 @@ class Message(name: String, text: String, time: Long) {
     public val userName = name;
     public val textMessage = text;
     private val messageTime = time;
-
-    /* public Message() {}
-    public Message(String userName, String textMessage) {
-        11:24 https://www.youtube.com/watch?v=Q9FxYm8-u7w&list=PL0lO_mIqDDFUXOmQ5c6hurb2Nqvc1uCxe&index=2
-    } */
 }
--- app/src/main/java/com/example/mainscreen/ui/fourth/BlankFragment.kt
+++ app/src/main/java/com/example/mainscreen/ui/fourth/BlankFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mainscreen.ui.fourth
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -8,12 +9,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ListView
 import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.mainscreen.BlankViewModel
 import com.example.mainscreen.Message
 import com.example.mainscreen.R
 import com.example.mainscreen.ui.notifications.PoliceIssues
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.database.FirebaseListAdapter
+import com.firebase.ui.database.FirebaseListOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
@@ -40,27 +46,58 @@ class BlankFragment : Fragment() {
         // TODO: Use the ViewModel
     }
 
-    // trying io connect to db
+    @SuppressLint("ResourceType")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        // trying to display messages; реализую с самого начала, с 8:47
+        // onActivityResult, 15:00 пропущу, не понял, зачем это итд => не буду реализовывать
+        // onCreate??
+        val SIGN_IN_CODE = 1
+        val fragmentBlank = view.findViewById<ConstraintLayout>(R.layout.fragment_blank)
+        val adapter : FirebaseListAdapter<Message>
         var buttonSend = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
         var editText = view.findViewById<TextInputEditText>(R.id.textInputEditText)
-        var ref = FirebaseDatabase.getInstance().getReference()
+        var ref = FirebaseDatabase.getInstance().reference
         ref.keepSynced(true)
 
+        // здесь все отлично работает
         buttonSend.setOnClickListener {
             var message = editText.text.toString().trim()
             if (message != "") {
                 val mess = Message("vlad", message, 126)
+                // логично иметь два пользователя: user и support?? или как это реализовать лучше? см тот гайд
                 ref.child("chat").push().setValue(mess);
             }
             editText.setText("")
         }
 
+        // это точно что-то нужное? или для этого еще и onActivityResult нужен, см верх?
         if (FirebaseAuth.getInstance().currentUser == null) {
-            // ?? https://www.youtube.com/watch?v=gFtajvZajrg&list=PL0lO_mIqDDFUXOmQ5c6hurb2Nqvc1uCxe&index=4 27:00
-            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(),  1)
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().build(), SIGN_IN_CODE)
         } else {
-            // hm
+            Snackbar.make(fragmentBlank, "Вы авторизованы", Snackbar.LENGTH_LONG).show()
+        }
+
+        // пока не дописал, разберусь с конструктором, допишу и норм запущу!
+        fun displayAllMessages() {
+            val listOfMessages = view.findViewById<ListView>(R.id.messages_list)
+
+            val options = FirebaseListOptions.Builder<Message>().setQuery(FirebaseDatabase.getInstance().reference, Message::class.java).setLayout(R.layout.messages_list_item,).build()
+            val adapter = object : FirebaseListAdapter<Message>(options) {
+                override fun populateView(v: View, model: Message, position: Int) {
+                    val mess_user : TextView
+                    var mess_time : TextView
+                    val mess_text : TextView
+                    mess_user = v.findViewById(R.id.message_user)
+                    mess_time = v.findViewById(R.id.message_time)
+                    mess_text = v.findViewById(R.id.message_text)
+
+                    mess_user.setText(model.userName)
+                    mess_text.setText(model.textMessage)
+                    mess_time.setText("0")
+                    // у меня нет time mess_time.setText(model.)
+                }
+            }
+            listOfMessages.setAdapter(adapter)
         }
     }
 
