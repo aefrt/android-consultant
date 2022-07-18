8c9dce44c27585b7bd759cce77066fc2c0befb43
aefrt <vkopchev43@gmail.com>
2022-07-18 11:13:08 +0300

    Продолжаю заниматься экранами 5–6, добавил нормальные ссылки, осталось доделать фронтенд

--- app/src/main/java/com/example/mainscreen/ui/notifications/PoliceIssues.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/PoliceIssues.kt
@@ -1,16 +1,109 @@
 package com.example.mainscreen.ui.notifications
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mainscreen.R
 
+
 class PoliceIssues : AppCompatActivity() {
+    // Экран 6
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_police_issues)
+
+        // 1
+        val osmotr_ss = SpannableString("1. Осмотр — это визуальное обследование со стороны, его основаниями могут быть информация об использовании транспортного средства в противоправных целях, необходимость проверки номеров, перевозимого груза. По окончании составляется акт осмотра.")
+        val clickableSpanOsmotr: ClickableSpan = object : ClickableSpan() {
+
+            override fun onClick(textView: View) {
+
+                startActivity(Intent(this@PoliceIssues, VzyatkaVymog::class.java))
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.setColor(Color.MAGENTA)
+                ds.isUnderlineText = false
+            }
+        }
+        osmotr_ss.setSpan(clickableSpanOsmotr, 3, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val boldSpanOsmotr = StyleSpan(Typeface.BOLD)
+        osmotr_ss.setSpan(boldSpanOsmotr, 3, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+
+        val osmotr = findViewById<TextView>(R.id.textView32)
+        osmotr.text = osmotr_ss
+        osmotr.movementMethod = LinkMovementMethod.getInstance()
+        osmotr.highlightColor = Color.TRANSPARENT
+
+        // 2
+        val vskrytie_ss = SpannableString("2. Вскрытие проводится, если есть подозрения, что в транспортном средстве находятся без специального разрешения предметы или вещи, изъятые из оборота или ограниченные в обороте.")
+        val clickableSpanVskrytie: ClickableSpan = object : ClickableSpan() {
+
+            override fun onClick(textView: View) {
+
+                startActivity(Intent(this@PoliceIssues, VzyatkaVymog::class.java))
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.setColor(Color.MAGENTA)
+                ds.isUnderlineText = false
+            }
+        }
+        vskrytie_ss.setSpan(clickableSpanVskrytie, 3, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val boldSpanVskrytie = StyleSpan(Typeface.BOLD)
+        vskrytie_ss.setSpan(boldSpanVskrytie, 3, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+
+        val vskrytie = findViewById<TextView>(R.id.textView60)
+        vskrytie.text = vskrytie_ss
+        vskrytie.movementMethod = LinkMovementMethod.getInstance()
+        vskrytie.highlightColor = Color.TRANSPARENT
+
+        // 3
+        val dosmotr_ss = SpannableString("3. Досмотр — обследование, проводимое без нарушения конструктивной целостности автомобиля для обнаружения орудий совершения либо предметов административного правонарушения. Это более серьезная процедура, требующая привлечения 2 понятых, либо применение видеозаписи. По окончании составляется протокол досмотра, либо вносится запись в протокол об административном правонарушении. Помните, что инспектор не может сам открывать двери и двигать предметы в транспортном средстве при досмотре.")
+        val clickableSpanDosmotr: ClickableSpan = object : ClickableSpan() {
+
+            override fun onClick(textView: View) {
+
+                startActivity(Intent(this@PoliceIssues, VzyatkaVymog::class.java))
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.setColor(Color.MAGENTA)
+                ds.isUnderlineText = false
+            }
+        }
+        dosmotr_ss.setSpan(clickableSpanDosmotr, 3, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        val boldSpanDosmotr = StyleSpan(Typeface.BOLD)
+        dosmotr_ss.setSpan(boldSpanDosmotr, 3, 10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+
+        val dosmotr = findViewById<TextView>(R.id.textView59)
+        dosmotr.text = dosmotr_ss
+        dosmotr.movementMethod = LinkMovementMethod.getInstance()
+        dosmotr.highlightColor = Color.TRANSPARENT
     }
+
+    // Другие экраны
 
     fun poliss(view: View){
         intent = Intent(this, Police_issues2::class.java)
@@ -34,7 +127,4 @@ class PoliceIssues : AppCompatActivity() {
         startActivity(intent)
     }
 
-    // Экран 6
-    // https://www.geeksforgeeks.org/how-to-make-substring-of-a-textview-clickable-in-android/ <- вот так будет достаточно
-    // Osmotr, Vskrytie, Dosmotr
 }
