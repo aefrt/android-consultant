95be1e956b30278ed15dbb4b589f6249b54f9ae3
aefrt <vkopchev43@gmail.com>
2022-07-19 16:00:15 +0300

    Сделана базовая структура экранов 1–3

--- app/src/main/java/com/example/mainscreen/ui/notifications/Extra.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Extra.kt
@@ -1,12 +1,29 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class Extra : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extra)
+    }
+
+    fun more1(view: View) {
+        intent = Intent(this, More1::class.java)
+        startActivity(intent)
+    }
+
+    fun more2(view: View) {
+        intent = Intent(this, More2::class.java)
+        startActivity(intent)
+    }
+
+    fun more3(view: View) {
+        intent = Intent(this, More3::class.java)
+        startActivity(intent)
     }
 }
