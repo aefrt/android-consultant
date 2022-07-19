cedba81d8bd24d3e670b34625867411fedca27b0
kirill867 <95954756+kirill867@users.noreply.github.com>
2022-07-19 17:19:16 +0300

    Добавлено обновлении инструкции о том, что делать, если остановил сотрудник ГИБДД

--- app/src/main/java/com/example/mainscreen/ui/notifications/Extra.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Extra.kt
@@ -1,7 +1,9 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class Extra : AppCompatActivity() {
@@ -9,4 +11,30 @@ class Extra : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extra)
     }
+
+    fun carextranumberfour(view: View) {
+        intent = Intent(this, Car_extra_number4::class.java)
+        startActivity(intent)
+    }
+
+    fun carextranumberfive(view: View) {
+        intent = Intent(this, Car_extra_number5::class.java)
+        startActivity(intent)
+    }
+
+    fun carextranumbersixone(view: View) {
+        intent = Intent(this, Car_extra_number6::class.java)
+        startActivity(intent)
+    }
+
+    fun carextranumbersixtwo(view: View) {
+        intent = Intent(this, Car_extra_number62::class.java)
+        startActivity(intent)
+    }
+
+    fun carextranumbersixthree(view: View) {
+        intent = Intent(this, Car_extra_number63::class.java)
+        startActivity(intent)
+    }
+
 }
