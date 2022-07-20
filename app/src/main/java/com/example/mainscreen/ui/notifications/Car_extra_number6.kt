67b98e60b0f8cf8f31b38bd81921e5c682df6753
kirill867 <95954756+kirill867@users.noreply.github.com>
2022-07-20 18:15:13 +0300

    Добавлено обновлении инструкции о том, что делать, если остановил сотрудник ГИБДД

--- app/src/main/java/com/example/mainscreen/ui/notifications/Car_extra_number6.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Car_extra_number6.kt
@@ -1,12 +1,19 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class Car_extra_number6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_extra_number6)
+    }
+
+    fun carextrazakonsix(view : View) {
+        intent = Intent(this, Car_extra_zakon_six::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/mainscreen/ui/notifications/Car_extra_number62.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Car_extra_number62.kt
@@ -1,12 +1,18 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class Car_extra_number62 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_extra_number62)
+    }
+    fun carextrazakonsixtwo(view : View) {
+        intent = Intent(this, Car_extra_zakon_six_two::class.java)
+        startActivity(intent)
     }
 }
