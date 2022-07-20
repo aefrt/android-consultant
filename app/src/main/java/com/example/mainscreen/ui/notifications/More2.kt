b9f2154ce503a28e2b57784d78b8a478df8a5ff9
aefrt <vkopchev43@gmail.com>
2022-07-20 15:24:53 +0300

    Сделана кнопка "Мне выписывают штраф"

--- app/src/main/java/com/example/mainscreen/ui/notifications/More2.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/More2.kt
@@ -1,12 +1,19 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class More2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_more2)
+    }
+
+    fun fine_more2(view : View) {
+        intent = Intent(this, Fine::class.java)
+        startActivity(intent)
     }
 }
