44fa589b84681ef12a206230d53edaa60f23fc49
aefrt <vkopchev43@gmail.com>
2022-07-20 17:34:41 +0300

    Рубрикатор: экраны 1–4, пока не до конца

---
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Adm931.kt
@@ -0,0 +1,11 @@
+package com.example.mainscreen.ui.notifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Adm931 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adm931)
+    }
+}
--- app/src/main/java/com/example/mainscreen/ui/notifications/Car_extra_number4.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Car_extra_number4.kt
@@ -1,12 +1,19 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class Car_extra_number4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car_extra_number4)
+    }
+
+    fun law(view : View) {
+        intent = Intent(this, Adm931::class.java)
+        startActivity(intent)
     }
 }
