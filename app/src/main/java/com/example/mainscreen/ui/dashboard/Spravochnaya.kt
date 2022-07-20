800b0635203b204c7d6c87c9cbfa689d2284e27f
Dmitriy <100220518+DmitriyLapynin@users.noreply.github.com>
2022-07-20 17:05:53 +0300

    Сделал инструкции для полиции и скорой помощи

---
+++ app/src/main/java/com/example/mainscreen/ui/dashboard/Spravochnaya.kt
@@ -0,0 +1,12 @@
+package com.example.mainscreen.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mainscreen.R
+
+class Spravochnaya : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spravochnaya)
+    }
+}
