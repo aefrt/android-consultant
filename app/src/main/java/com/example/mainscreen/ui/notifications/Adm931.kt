b6f76e0fda08ca01eed7d0e7dbcafcc16d231f85
aefrt <vkopchev43@gmail.com>
2022-07-20 17:37:08 +0300

    Рубрикатор: экраны 1–4, пока не до конца

--- app/src/main/java/com/example/mainscreen/ui/notifications/Adm931.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/Adm931.kt
@@ -2,6 +2,7 @@ package com.example.mainscreen.ui.notifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mainscreen.R
 
 class Adm931 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
