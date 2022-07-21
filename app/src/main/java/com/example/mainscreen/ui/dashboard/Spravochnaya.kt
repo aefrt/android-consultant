9d3a71d987c657079509ca37cae448e40d58b215
Dmitriy <100220518+DmitriyLapynin@users.noreply.github.com>
2022-07-21 20:17:54 +0300

    Сделал активити со ссылкой на административные правонарушения

--- app/src/main/java/com/example/mainscreen/ui/dashboard/Spravochnaya.kt
+++ app/src/main/java/com/example/mainscreen/ui/dashboard/Spravochnaya.kt
@@ -1,22 +1,21 @@
 package com.example.mainscreen.ui.dashboard
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Switch
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mainscreen.R
+
 
 class Spravochnaya : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_spravochnaya)
-       /* val standardSwitch: Switch = findViewById(R.id.switch_compat)
+    }
 
-        standardSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
-            if (isChecked) {
-                println("Standard Switch is on")
-            } else {
-                println("Standard Switch is off")
-            }
-        }*/
+    fun link(view : View){
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://docs.google.com/document/d/1cp5pp1Bde6LwSY5TtVu6XOwrjH4ClUg0aeGOu24dBg4/edit"))
+        startActivity(browserIntent)
     }
 }
