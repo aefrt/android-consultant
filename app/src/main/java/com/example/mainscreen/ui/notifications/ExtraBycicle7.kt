7eeeaacd76c893d86a90dba3eb82ad475a78f5b8
aefrt <vkopchev43@gmail.com>
2022-07-21 23:26:21 +0300

    Велосипед, сделал экраны 5–8 без ссылок на 112 и статьи

--- app/src/main/java/com/example/mainscreen/ui/notifications/ExtraBycicle7.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/ExtraBycicle7.kt
@@ -1,12 +1,19 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class ExtraBycicle7 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extra_bycicle7)
+    }
+
+    fun bycicleProtocolHowTo(view : View) {
+        intent = Intent(this, Protocol::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/mainscreen/ui/notifications/ExtraBycicle8.kt
+++ app/src/main/java/com/example/mainscreen/ui/notifications/ExtraBycicle8.kt
@@ -1,12 +1,19 @@
 package com.example.mainscreen.ui.notifications
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.mainscreen.R
 
 class ExtraBycicle8 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extra_bycicle8)
+    }
+
+    fun bycicleFineHowTo(view : View) {
+        intent = Intent(this, IGetAFine::class.java)
+        startActivity(intent)
     }
 }
